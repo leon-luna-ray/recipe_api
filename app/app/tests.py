801a58975c9a04db53938a2c40b79b7739d7bdeb
Ray Luna <leon.luna.ray@gmail.com>
2023-02-02 22:21:43 -0800

    add sample tests

---
+++ app/app/tests.py
@@ -0,0 +1,22 @@
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+    """ Sample test cases """
+
+    def test_add_numbers(self):
+        """ Test adding numbers together """
+        res = calc.add(5, 5)
+
+        self.assertEqual(res, 10)
+    
+    def test_subtract_numbers(self):
+        """ Test subtracting numbers """
+        res = calc.subtract(100, 99)
+        self.assertEqual(res, 1)
