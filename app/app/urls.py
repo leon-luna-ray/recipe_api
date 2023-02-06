accf1680e95f22138864d1e24713bd5011feb273
Ray Luna <leon.luna.ray@gmail.com>
2023-02-05 17:16:22 -0800

    add swagger

--- app/app/urls.py
+++ app/app/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from drf_spectacular.views import (
     SpectacularAPIView,
-    SectacularSwaggerView,
+    SpectacularSwaggerView,
 )
 from django.contrib import admin
 from django.urls import path
@@ -26,7 +26,7 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
     path(
         'api/docs/',
-        SectacularSwaggerView.as_view(url_name='api-schema'),
+        SpectacularSwaggerView.as_view(url_name='api-schema'),
         name='api-docs',
     )
 ]
