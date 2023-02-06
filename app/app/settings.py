12d36dd0c0659ea7bc9bda91069cf219bca5e23d
Ray Luna <leon.luna.ray@gmail.com>
2023-02-05 17:11:36 -0800

    add spectacular def

--- app/app/settings.py
+++ app/app/settings.py
@@ -26,6 +26,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+    'drf_spectacular',
+
     # Project apps
     'core',
 ]
@@ -109,3 +112,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'
+}
